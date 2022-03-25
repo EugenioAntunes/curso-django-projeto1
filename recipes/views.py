58ecc0b55e7566fff8a1f78d9a74443ea90f8bf8
EugenioAntunes <eugenio.antunes@yahoo.com.br>
2022-03-25 09:32:06 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'apague/temp.html')
-
-def sobre(request):
-    return HttpResponse('sobre')

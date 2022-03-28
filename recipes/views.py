d5057bd24dba86623982cec2091ab018b2fc5859
EugenioAntunes <eugenio.antunes@yahoo.com.br>
2022-03-28 10:00:40 -0300

    Adicinando url e view para vizualizar receitas

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name':'Eugênio'})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipes.html', context={'name':'Eugênio'})

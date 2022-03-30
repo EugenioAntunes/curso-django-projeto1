d6586a9e981bd2f1619158984d8a55661fd8273a
EugenioAntunes <eugenio.antunes@yahoo.com.br>
2022-03-30 11:37:44 -0300

    Aplicando migrações - makemigrations and migrate

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,10 @@
-from distutils.command import upload
-from turtle import title
-from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65),
@@ -16,5 +19,6 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True),
     updated_at = models.DateTimeField(auto_now=True),
     is_published = models.BooleanField(default=False),
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
-    
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d'),
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True),
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)

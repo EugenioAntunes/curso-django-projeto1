0ebbbe1a54a5aa6ea58990a60f2689df9060f89b
EugenioAntunes <eugenio.antunes@yahoo.com.br>
2022-03-31 09:26:15 -0300

    Adicinando dados no admin

--- projeto/settings.py
+++ projeto/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = 'static/'
 STATICFILE_DIRS = [
     os.path.join(BASE_DIR, 'recipes/static')
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

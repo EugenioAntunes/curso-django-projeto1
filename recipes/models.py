4ec4ef742dc50c0ebc2c69c8daea1d8d53ba0ff7
EugenioAntunes <eugenio.antunes@yahoo.com.br>
2022-04-11 09:30:03 -0300

    Adicionando Receitas pelo admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,4 @@
-
-from ssl import Options
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -16,14 +15,14 @@ class Recipe(models.Model):
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=50)
-    srvings_time = models.IntegerField()
+    servings_time = models.IntegerField()
     servings_unit = models.CharField(max_length=50)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    #created_at = models.DateTimeField(default=datetime.now, blank=True)
+    #updated_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d', default=None)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
